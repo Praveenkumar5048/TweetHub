7ea11ff08046b8ce5b13ec94eb73117dc42bd86b
Praveenkumar5048 <praveenkumarhireboodur@gmail.com>
2024-02-02 21:23:14 +0530

    database update

--- Backend/schema.sql
+++ Backend/schema.sql
@@ -1,11 +1,11 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS College;
+CREATE DATABASE IF NOT EXISTS student;
 
 -- Switch to the specified database
 USE student;
 
 -- Create the student_info table if it doesn't exist
-CREATE TABLE IF NOT EXISTS Fuels (
+CREATE TABLE IF NOT EXISTS student_info (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
