d925e11bcdbedbed87485de2ed87bde483ceeaff
Sag@0418 <sagarathani0418@gmail.com>
2024-02-06 19:12:13 +0530

    updated the schema

--- Backend/database.js
+++ Backend/database.js
@@ -1,28 +1,31 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Create a single pool and reuse it
 const db = mysql.createPool({
-    host:process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DATABASE
-}).promise()
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
 
-export async function getnotes(){
-    const [rows] = await db.query("SELECT * FROM student_info")
+export async function getUsers() {
+    const [rows] = await db.query('SELECT * FROM Users');
     return rows;
 }
 
-export async function getnote(id){
-    const [row] = await db.query(
-        'SELECT * FROM student_info where student_id = ?', [id])
-    return row;    
+export async function getPosts() {
+    const [rows] = await db.query('SELECT * FROM Posts');
+    return rows;
 }
 
-export async function createnote(student_id, first_name, last_name, age, major){
-    const response = await db.query('INSERT INTO student_info (student_id, first_name, last_name, age, major) VALUES (?, ?, ?, ?, ?)',
-    [student_id, first_name, last_name, age, major])
-    return response;
+export async function getLikes() {
+    const [rows] = await db.query('SELECT * FROM Likes');
+    return rows;
 }
 
+export async function getComments() {
+    const [rows] = await db.query('SELECT * FROM Comments');
+    return rows;
+}
