b0a8f7e3ddb45c060081575ee71a5985167fda90
Praveenkumar5048 <praveenkumarhireboodur@gmail.com>
2024-02-24 22:38:46 +0530

    add profile page

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -8,7 +8,6 @@ export const loginUser  = async (req, res) => {
         
         const getUserQuery = `SELECT * FROM Users WHERE email = ?`;
         const userData = await db.query(getUserQuery, [email]);
-        console.log(userData)
         if (userData[0].length !== 0) {
             const user = userData[0][0];
             
